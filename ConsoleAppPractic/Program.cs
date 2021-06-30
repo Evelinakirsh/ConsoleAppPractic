fe9819f40bb45155d21a36f7796b1e2160e57373
Эвелина Кирштейн <eveli2001@icloud.com>
2021-06-30 17:44:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppPractic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPractic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my friend");
+            Console.ReadKey();
+        }
+    }
+}
